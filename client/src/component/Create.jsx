9d082eeaad84dd5e3b402ffb76f89056c4dc82dc
Parkseonho <tjsgh2103@gmail.com>
2022-10-06 23:15:53 +0900

    연결 오류 해결 관련 설명

--- client/src/component/Create.jsx
+++ client/src/component/Create.jsx
@@ -16,15 +16,18 @@ const Create = () => {
        e.preventDefault();
        const data = await axios({
         url: "http://localhost:3000/create",
+        // 위의 주소는 5000번대가 아닌 3000번대로 (전송주소!)
         method: "POST",
         data: {
           title, 
           content
-        },
-        withCredentials: true,
-       });      
+        }
+       });     
+
        if(data.data!==null){
         nav("/");
+        //navigate 기능을 활용하여 Access-Control-Allow-Origin 관련 오류를 막을 수 있음?
+        // 그냥 보내면 server에서 create를 못읽음
         console.log("성공");
        }else{
         console.log("오류");
@@ -38,7 +41,6 @@ const Create = () => {
               type="text"
               placeholder="제목"
               value={title}
-              name="title"
               onChange={(e) => {
                 setTitle(e.target.value);
               }}
@@ -53,7 +55,6 @@ const Create = () => {
               type="text"
               placeholder="내용"
               value={content}
-              name="content"
               onChange={(e) => {
                 setContent(e.target.value);
               }}
--- server/server.js
+++ server/server.js
@@ -6,16 +6,15 @@ const bodyParser = require("body-parser");
 
 app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
   next();
 });
 
 app.use(bodyParser.json());
+// 이게 있어야지 Create.jsx에서 path로 값을 보낼 수가 있음
+// 없으면 request.body undefined 오류뜸
+// 순서 유의! path 뒤에 가면 안먹어서 오류뜸
 
 app.use("/", path);
-
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 const port = 5000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
 app.listen(port, () => {
